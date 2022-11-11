27aec95f7b998e045036c163540fed76757b3990
DuoSince <96527050+DuoSince@users.noreply.github.com>
2022-11-11 17:12:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -31,7 +31,7 @@ global.APIKeys = { // APIKey Here
 }
 global.packname = data.namabot
 global.author = data.owner
-global.wm = data.namabot + ' ' + data.owner
+global.wm = 'DevXyZ'
 
 global.multiplier = 100 // The higher, The harder levelup
 
