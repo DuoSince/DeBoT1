1b0bfca97c3c62e78f07f776ac82a35340938660
DuoSince <96527050+DuoSince@users.noreply.github.com>
2022-11-12 09:52:46 +0700

    Update i-tqto.js

--- plugins/i-tqto.js
+++ plugins/i-tqto.js
@@ -8,37 +8,12 @@ let handler = async (m, { conn, usedPrefix }) => {
     conn.relayMessage(m.chat,  {
     requestPaymentMessage: {
       currencyCodeIso4217: 'INR',
-      amount1000: 1339889,
+      amount1000: 'DevXyZ',
       requestFrom: m.sender,
       noteMessage: {
       extendedTextMessage: {
       text: `
- *BIG THANKS*
-Nurutomo: 
-https://github.com/Nurutomo
-Istikmal: 
-https://github.com/BochilGaming
-Ariffb: 
-https://github.com/Ariffb25
-Aguz Familia:
-https://github.com/FokusDotId
-Amelia Lisa:
-https://github.com/Ameliascrf
-Aniq12
-https://github.com/aniq12
-Ilman: 
-https://github.com/ilmanhdyt
-Amirul: 
-https://github.com/amiruldev20
-Irwan:
-https://github.com/irwanx
-Rasel:
-https://github.com/raselcomel
-Arull:
-https://github.com/Warikrr
-
-\`\`\`${conn.user.name}\`\`\`
-`,
+      *DevXyZ x Queenzy Bot*`,
       contextInfo: {
       externalAdReply: {
       showAdAttribution: true
