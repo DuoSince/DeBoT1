0344965d15f96344373ac29b6a41e8f3db623274
Komang <96527050+Komang05@users.noreply.github.com>
2022-11-23 19:32:31 +0700

    Update _samp.js

--- plugins/_samp.js
+++ plugins/_samp.js
@@ -1,1 +1,24 @@
 
+let fetch = require('node-fetch')
+let handler = async(m, { conn, text }) => {
+    if (!text) throw 'masukin judulnya!'
+    let yt = await fetch(`http://anabellebot.online/API/sampquery?ip=${text}&port=${text}`)
+    let json = await yt.json()
+    if (!json.status) throw json
+    let js = json.result
+    let txt = `
+[ *PLAY MUSIC* ]
+*title:* ${hostname}
+*size:* ${js.sizeAudio}
+link ${js.link}
+
+_*sabar audio akan dikirim*_
+`
+    conn.sendFile(m.chat, js.thumb, '', txt, m)
+    conn.sendFile(m.chat, json.result.getAudio, '', '', m)
+}
+handler.help = ['play']
+handler.tags = ['downloader']
+handler.command = /^(ytp|samp)$/i
+
+module.exports = handler
